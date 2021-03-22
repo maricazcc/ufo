a8a00076f3371ccb3e0204ec6ac83573f6e50652
Marica Zocco <Marica Zocco@Marica.homenet.telecomitalia.it>
2021-03-22 10:55:53 +0100

    PreparedStatement

---
+++ src/main/java/it/polito/tdp/ufo/db/TestDB.java
@@ -0,0 +1,53 @@
+package it.polito.tdp.ufo.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestDB {
+	
+	public static void main (String[] args) {
+		
+		String jdbcURL = "jdbc:mysql://localhost/ufo_sightings?user=root&password=root";
+		
+		try {
+			Connection conn = DriverManager.getConnection(jdbcURL);	
+			String sql = "SELECT DISTINCT shape FROM sighting WHERE shape<>'' ORDER BY shape ASC";	
+			PreparedStatement st = conn.prepareStatement(sql);			
+					
+			ResultSet res = st.executeQuery(sql);
+			
+			List<String> formeUFO = new ArrayList<>();
+			
+			while (res.next()) {
+				String forma = res.getString("shape");
+				formeUFO.add(forma);
+			}
+			
+			st.close();
+			System.out.println(formeUFO);	
+			
+			String sql2 = "SELECT COUNT(*) AS cnt FROM sighting WHERE shape = ? ";
+			String shapeScelta = "circle";
+			PreparedStatement st2 = conn.prepareStatement(sql2);
+			st2.setString(1, shapeScelta);
+			ResultSet res2 = st2.executeQuery();
+			res2.first(); //essendo di tipo count siamo sicuri che si tratti di una sola riga, il ciclo while sarebbe uno spreco
+			int count = res2.getInt("cnt");
+			st2.close();
+			System.out.println("UFO di forma "+shapeScelta+" sono: "+count);
+			
+			conn.close();
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+}
